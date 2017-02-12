caab787298bd6fb3d1707bb3705294e6e4239832
UnnikrishnanSasikumar <us57@njit.edu>
2017-02-11 19:24:30 -0500

    Newly Formatted variables

--- display_discount.php
+++ display_discount.php
@@ -1,3 +1,4 @@
+
 <?php
     // get the data from the form
     $product_description = filter_input(INPUT_POST,'product_description');
@@ -28,19 +29,19 @@
 	<h1>Product Discount Calculator</h1>
 
         <label>Product Description:</label>
-        <span><?php echo $product_description_escaped; ?></span><br>
+        <span><?php echo htmlspecialchars($product_description); ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo $list_price_formatted; ?></span><br>
+        <span><?php echo htmlspecialchars($list_price_f); ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo $discount_percent_formatted; ?></span><br>
+        <span><?php echo htmlspecialchars($discount_percent_f); ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo $discount_formatted; ?></span><br>
+        <span><?php echo $discount_f; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo $discount_price_formatted; ?></span><br>
+        <span><?php echo $discount_price_f; ?></span><br>
     </main>
 </body>
 </html>
